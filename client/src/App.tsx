5083effe1e0a9f2787988e8e4316ed70449ac61b
sotliqovjavohir4151-droid <sotliqovjavohir4151@gmail.com>
2026-02-15 20:16:15 +0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "./components/ui/toaster"
 import { BottomNav } from "@/components/BottomNav";
 import { ProgressProvider } from "@/context/ProgressContext";
 
